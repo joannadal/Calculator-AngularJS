8e9ac7d03b98ac00c463cafb50bae90ae39f1eee
Joan <joan.nadal.brotat@gmail.com>
2015-06-14 18:27:45 +0100

    Added decimals and the option to save a result

--- js/calculator.js
+++ js/calculator.js
@@ -4,6 +4,7 @@ angular.module("Calculator", []).controller("CalculatorController",
         $scope.memory = null;
         $scope.result = 0;
         $scope.operation = null;
+        $scope.savedMemory = null;
 
         $scope.addNumber = function(newNumber) {
             if ($scope.displayValue.length < 18) {
@@ -42,6 +43,37 @@ angular.module("Calculator", []).controller("CalculatorController",
             $scope.operation = "/";
             $scope.displayValue = '';
         };
+        $scope.onClickChangeSign = function() {
+            if ($scope.displayValue > 0) {
+                $scope.displayValue = -$scope.displayValue;
+            }
+            else if ($scope.displayValue < 0) {
+                $scope.displayValue *= -1;
+            }
+            $scope.result = $scope.displayValue;
+            $scope.memory = $scope.result;
+            $scope.displayValue = $scope.result;
+        };
+        $scope.onClickPercentage = function() {
+            $scope.result = $scope.displayValue/100;
+            $scope.operation = null;
+            if ($scope.result != 0) {
+                $scope.memory = $scope.result;
+            }
+            if ($scope.result != 0) {
+                $scope.displayValue = $scope.result;
+            }
+        };
+        $scope.onClickSaveMemory = function() {
+            if ($scope.savedMemory != '') {
+                $scope.savedMemory = $scope.displayValue;
+            }
+        }
+        $scope.onClickReadMemory = function() {
+            if ($scope.savedMemory != null) {
+                $scope.displayValue = $scope.savedMemory;
+            }
+        }
         $scope.onClickCalculate = function() {
             if ($scope.operation == "+"){
                 $scope.result = parseFloat($scope.memory) + parseFloat($scope.displayValue);
